b507d299fb06fdf038b825f1db89353a2cb9816f
pearlsky <pearlakpan115@gmail.com>
2023-05-01 12:09:06 +0100

    fix: update imports to fix errors

--- src/pages/overview.jsx
+++ src/pages/overview.jsx
@@ -1,5 +1,9 @@
-import Header from "@/components/dashboard/header";
-import { Status, YourLessons, Leaderboard } from "@/components/dashboard";
+import {
+  Header,
+  Status,
+  YourLessons,
+  Leaderboard,
+} from "@/components/dashboard";
 
 const Overview = () => (
   <div className="flex flex-col gap-7">
