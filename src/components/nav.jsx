0c89243757764f6688f893eee77af5e6a2520f81
pearlsky <pearlakpan115@gmail.com>
2023-05-01 12:17:48 +0100

    refactor(nav): write active states for nav links

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import {
   HiOutlineUsers,
   HiOutlineChartBar,
@@ -6,29 +6,63 @@ import {
 } from "react-icons/hi2";
 import { HiOutlineHome } from "react-icons/hi";
 
+// note: (refactor): render nav items dynamically
+
 const Nav = () => (
   <div className="fixed bottom-0 left-0 right-0 max-w-[400px] w-full mx-auto">
     <section className="py-6 px-8 bg-white rounded-t-3xl">
       <ul className="flex justify-between">
         <li>
-          <Link to="/">
-            <HiOutlineHome className="text-2xl text-gray-400" />
-          </Link>
+          <NavLink to="/">
+            {({ isActive }) => (
+              <HiOutlineHome
+                className={
+                  isActive
+                    ? "text-2xl text-pryblue fill-pryblue"
+                    : "text-2xl text-gray-400"
+                }
+              />
+            )}
+          </NavLink>
         </li>
         <li>
-          <Link to="/learning-progress">
-            <HiOutlineChartBar className="text-2xl text-gray-400" />
-          </Link>
+          <NavLink to="/learning-progress">
+            {({ isActive }) => (
+              <HiOutlineChartBar
+                className={
+                  isActive
+                    ? "text-2xl text-pryblue fill-pryblue"
+                    : "text-2xl text-gray-400"
+                }
+              />
+            )}
+          </NavLink>
         </li>
         <li>
-          <Link to="/profile">
-            <HiOutlineUsers className="text-2xl text-gray-400" />
-          </Link>
+          <NavLink to="/profile">
+            {({ isActive }) => (
+              <HiOutlineUsers
+                className={
+                  isActive
+                    ? "text-2xl text-pryblue fill-pryblue"
+                    : "text-2xl text-gray-400"
+                }
+              />
+            )}
+          </NavLink>
         </li>
         <li>
-          <Link to="/settings">
-            <HiOutlineCog6Tooth className="text-2xl text-gray-400" />
-          </Link>
+          <NavLink to="/settings">
+            {({ isActive }) => (
+              <HiOutlineCog6Tooth
+                className={
+                  isActive
+                    ? "text-2xl text-pryblue fill-pryblue"
+                    : "text-2xl text-gray-400"
+                }
+              />
+            )}
+          </NavLink>
         </li>
       </ul>
     </section>
